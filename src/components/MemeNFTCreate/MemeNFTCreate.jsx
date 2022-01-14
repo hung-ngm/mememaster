484f949a4dbf6ba9160f12720bff936601cbb1e1
minhhung123 <prohungnguyenminh@gmail.com>
2022-01-14 20:31:16 +0700

    Add price form

--- src/components/MemeNFTCreate/MemeNFTCreate.jsx
+++ src/components/MemeNFTCreate/MemeNFTCreate.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Typography } from 'antd';
-import { Row, Col, Upload, message } from 'antd';
+import React, { useState } from 'react'
+import { Row, Col, Upload, message, Typography, Input, Tooltip } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 
 const { Dragger } = Upload;
 const { Title } = Typography;
+
+const formatNumber = (value) => {
+  value += '';
+  const list = value.split('.');
+  const prefix = list[0].charAt(0) === '-' ? '-' : '';
+  let num = prefix ? list[0].slice(1) : list[0];
+  let result = '';
+  while (num.length > 3) {
+    result = `,${num.slice(-3)}${result}`;
+    num = num.slice(0, num.length - 3);
+  }
+  if (num) {
+    result = num + result;
+  }
+  return `${prefix}${result}${list[1] ? `.${list[1]}` : ''}`;
+}
 
 const styles = {
   fileUpload: {
@@ -12,6 +27,9 @@ const styles = {
     paddingRight: "15px",
     justifyContent: "center",
     alignItems: "center",
+  },
+  titleWrapper: {
+    padding: "10px",
   }
 }
 
@@ -20,7 +38,9 @@ const props = {
   multiple: true,
   action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
   onChange(info) {
+    console.log(info);
     const { status } = info.file;
+    console.log(status);
     if (status !== 'uploading') {
       console.log(info.file, info.fileList);
     }
@@ -35,16 +55,67 @@ const props = {
   },
 };
 
+const NumericInput = (props) => {
+  const onPriceChange = e => {
+    const { value } = e.target;
+    const reg = /^-?\d*(\.\d*)?$/;
+    if ((!isNaN(value) && reg.test(value)) || value === '' || value === '-') {
+      props.onChange(value);
+    }
+  };
+
+  // '.' at the end or only '-' in the input box.
+  const onBlur = () => {
+    const { value, onBlur, onChange } = props;
+    let valueTemp = value;
+    if (value.charAt(value.length - 1) === '.' || value === '-') {
+      valueTemp = value.slice(0, -1);
+    }
+    onPriceChange(valueTemp.replace(/0*(\d+)/, '$1'));
+    if (onBlur) {
+      onBlur();
+    }
+  };
+  const { value, onChange } = props;
+  const title = value ? (
+    <span className="numeric-input-title">{value !== '-' ? formatNumber(value) : '-'}</span>
+  ) : (
+    'Input a number'
+  );
+  return (
+    <Tooltip
+      trigger={['focus']}
+      title={title}
+      placement="topLeft"
+      overlayClassName="numeric-input"
+    >
+      <Input
+        
+        onChange={onChange}
+        onBlur={onBlur}
+        placeholder="Input a number"
+        maxLength={25}
+      />
+    </Tooltip>
+  );
+
+}
+
 const MemeNFTCreate = () => {
+  const [value, setValue] = useState(0);
+  const onChange = (value) => {
+    setValue(value);
+  }
+  
   return (
     <div>
       <Title>Create your meme as an NFT</Title>
       <Row>
         <Col span={18}>
           <Row>
-            <Title level={4}>Upload file</Title>
-            <Col span={20} offset={2}>
-              <div style={styles.fileUpload}>
+            <div id="fileUpload" style={styles.titleWrapper}>
+              <Title level={4}>Upload file</Title>
+              <Col span={20} offset={2} style={styles.fileUpload}>
                 <Dragger {...props}>
                   <p className="ant-upload-drag-icon">
                     <InboxOutlined />
@@ -54,9 +125,20 @@ const MemeNFTCreate = () => {
                     Support for a single or bulk upload. Strictly prohibit from uploading company data or other
                     band files
                   </p>
-                </Dragger>
-              </div>
-            </Col>
+                  </Dragger>    
+              </Col>
+            </div>
+
+            <div id="price" style={styles.titleWrapper}>
+              <Title level={4}>Price</Title>
+              <Col span={24}>
+                <NumericInput style={{ width: '100%' }} value={value} onChange={onChange} />
+              </Col>
+              
+            </div>
+            <div id="name"></div>
+            <div id="description"></div>
+            
           </Row>
         </Col>
         <Col span={6}>Preview</Col>
